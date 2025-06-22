c330f7f89b69143699bfb93b3214867a3c22da71
Syed-Moinuddin2025 <s4moin123@gmail.com>
2025-06-22 10:40:17 +0530

    02_Global_Scope

---
+++ python_basics/06_scope/02_global_scope.py
@@ -0,0 +1,13 @@
+name = "Syed"
+
+def greet():
+    print("Hello", name)
+
+greet()
+print("Outside also:", name)
+print("-----------------------------------------------------------")
+username = "chaiaurcode"
+def func() :
+    username = "chai"
+func()
+print(username)
