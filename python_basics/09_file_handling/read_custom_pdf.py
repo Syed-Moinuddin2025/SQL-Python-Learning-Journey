04e73171c143bb4f0df5856c237bb1fbb42b6d3b
Syed-Moinuddin2025 <s4moin123@gmail.com>
2025-06-23 16:19:50 +0530

    Read Custme PDF

---
+++ python_basics/09_file_handling/read_custom_pdf.py
@@ -0,0 +1,12 @@
+import PyPDF2
+
+filename = "Python-Notes.pdf"
+
+with open(filename, "rb") as file:
+    reader = PyPDF2.PdfReader(file)
+
+    for page_num, page in enumerate(reader.pages):
+        text = page.extract_text()
+        print(f"📄 Page {page_num + 1} content:")
+        print(text)
+        print("-" * 50)
