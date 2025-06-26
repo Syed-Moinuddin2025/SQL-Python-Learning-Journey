5a4f8c745365d72f0d86605118c83351213228c6
Syed-Moinuddin2025 <s4moin123@gmail.com>
2025-06-26 20:20:21 +0530

    Squre

--- python_basics/05_functions_basics/01_square_function.py
+++ python_basics/05_functions_basics/01_square_function.py
@@ -5,10 +5,10 @@
 def square(number):
     return number ** 2
   
-result = square(4)
+result = square(5)
 print(result)
 
-print(square(10))  # Output: 100
-print(square(4))   # Output: 16
-print(square(0))   # Output: 0  
-print(square(-3))  # Output: 9
+# print(square(10))  # Output: 100
+# print(square(4))   # Output: 16
+# print(square(0))   # Output: 0  
+# print(square(-3))  # Output: 9
