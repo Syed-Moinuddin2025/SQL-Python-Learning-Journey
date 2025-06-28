f6eb62f3c418d636682ebe81c7bc4fe113bf34bf
Syed-Moinuddin2025 <s4moin123@gmail.com>
2025-06-28 22:25:48 +0530

    Practive file

--- The movie rating/movies_rating_practice.sql
+++ The movie rating/movies_rating_practice.sql
@@ -23,3 +23,37 @@ UPDATE movies
 SET movie_title = 'The Professional (Léon)' 
 WHERE id = 25;
  
+ALTER TABLE movies
+ADD CONSTRAINT fk_movies_d_company
+FOREIGN KEY (d_company_id)
+REFERENCES d_company(id);
+
+sp_help movies;
+sp_help d_company;
+
+ALTER TABLE movies
+ALTER COLUMN d_company_id INT;
+
+ALTER TABLE movies
+ADD CONSTRAINT fk_movies_d_company
+FOREIGN KEY (d_company_id)
+REFERENCES d_company(id);
+
+
+
+SELECT TOP 10
+    m.movie_title,
+    d.company_name
+FROM movies m
+JOIN d_company d ON m.d_company_id = d.id;
+
+SELECT TOP 20
+    m.id AS movie_id,
+    m.movie_title,
+    m.year_released,
+    d.company_name,
+    m.imdb_rating,
+    m.language
+FROM movies m
+JOIN d_company d ON m.d_company_id = d.id
+ORDER BY m.year_released;
